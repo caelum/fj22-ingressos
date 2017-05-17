d5a1631f01b9ccff4fe5af5e0b118f7d4da4b1f1
Jonathan <jonathan.pontes@hotmail.com>
2017-05-16 21:50:49 -0300

    Commit com as Sessões criadas

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -4,6 +4,9 @@ package br.com.caelum.ingresso.dao;
 //import br.com.caelum.ingresso.model.Sessao;
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -14,17 +17,15 @@ import java.util.List;
 @Repository
 public class SessaoDao {
 
-//    @PersistenceContext
-//    private EntityManager manager;
-//
-//
-//    public void save(Sessao sessao) {
-//        manager.persist(sessao);
-//    }
-//
-//    public List<Sessao> buscaSessoesDaSala(Sala sala) {
-//        return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class)
-//                .setParameter("sala", sala)
-//                .getResultList();
-//    }
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void save(Sessao sessao) {
+		manager.persist(sessao);
+	}
+
+	public List<Sessao> buscaSessoesDaSala(Sala sala) {
+		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class)
+				.setParameter("sala", sala).getResultList();
+	}
 }
