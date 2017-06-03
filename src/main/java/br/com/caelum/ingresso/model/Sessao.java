7d2f15d3cd0c8908cf09c431c14b06bb8be2949f
gCast <casteglioni@gmail.com>
2017-06-03 16:08:08 -0300

    incluido model da Sessao

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,66 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by nando on 03/03/17.
+ */
+@Entity
+public class Sessao {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private LocalTime horario;
+    
+    @ManyToOne
+    private Sala sala;
+    @ManyToOne
+    private Filme filme;
+    
+    public Sessao(){}
+    
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
+		this.horario = horario;
+		this.setFilme(filme);
+		this.sala = sala;
+	}
+    
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	
+	public LocalTime getHorarioTermino() {
+		return this.horario.plus(filme.getDuracao().toMinutes(), ChronoUnit.MINUTES);
+	}
+    
+}
