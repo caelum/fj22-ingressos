149d623560ee83f7562c4862cad81c4d7cae89be
Claudio Domingues <claudio.domingues68@hotmail.com>
2019-01-29 09:47:08 -0200

    Arrumei o @Entity na Sessao.java

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.Entity;
+
+@Entity
 public class Sessao {
 	
 	
