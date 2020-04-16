ee14612041742c1901a6108ef5d18a1b8e86e02b
Rodrigo Cruz <drigo.ccruz@gmail.com>
2020-04-16 20:17:22 -0300

    Corrigindo a variavel filmeid para filmeId, para todos metodos

--- src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
+++ src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
@@ -18,14 +18,14 @@ public class SessaoForm {
 	private Integer salaId;
 
 	@NotNull
-	private Integer filmeid;
+	private Integer filmeId;
 
 	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull
 	private LocalTime horario;
 
 	public Sessao criaSessao(SalaDao salaDao, FilmeDao filmeDao) {
-		Filme filme = filmeDao.findOne(filmeid); 
+		Filme filme = filmeDao.findOne(filmeId); 
 		Sala sala = salaDao.findOne(salaId);
 	  
 		Sessao sessao = new Sessao(this.horario, filme, sala);
@@ -41,12 +41,12 @@ public class SessaoForm {
 		this.salaId = salaId;
 	}
 
-	public Integer getFilmeid() {
-		return filmeid;
+	public Integer getFilmeId() {
+		return filmeId;
 	}
 
-	public void setFilmeid(Integer filmeid) {
-		this.filmeid = filmeid;
+	public void setFilmeId(Integer filmeId) {
+		this.filmeId = filmeId;
 	}
 
 	public LocalTime getHorario() {
